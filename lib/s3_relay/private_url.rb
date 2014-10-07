2a948c7e69dd6b83e91beef1f9b7f5b9b7427905
Kenny Johnston <kjohnston@appcreations.com>
2014-10-07 16:25:34 -0700

    Add PrivateUrl class to generate signed URLs.

---
+++ lib/s3_relay/private_url.rb
@@ -0,0 +1,32 @@
+module S3Relay
+  class PrivateUrl < S3Relay::Base
+
+    attr_reader :expires, :path
+
+    def initialize(public_url, options={})
+      @path    = URI.parse(public_url).path
+      @expires = (options[:expires] || 1.minute.from_now).to_i
+    end
+
+    def generate
+      "#{endpoint}#{path}?#{params}"
+    end
+
+    private
+
+    def params
+      [
+        "AWSAccessKeyId=#{ACCESS_KEY_ID}",
+        "Expires=#{expires}",
+        "Signature=#{signature}"
+      ].join("&")
+    end
+
+    def signature
+      string = "GET\n\n\n#{expires}\n/#{BUCKET}#{path}"
+      hmac   = OpenSSL::HMAC.digest(digest, SECRET_ACCESS_KEY, string)
+      CGI.escape(Base64.encode64(hmac).strip)
+    end
+
+  end
+end
