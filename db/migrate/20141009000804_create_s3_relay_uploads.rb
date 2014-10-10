7c00c8e2f4790896d7ecf07e6edd428c7247c2e3
Kenny Johnston <kjohnston.ca@gmail.com>
2014-10-10 00:37:05 -0700

    Fix data type for uuid.

--- db/migrate/20141009000804_create_s3_relay_uploads.rb
+++ db/migrate/20141009000804_create_s3_relay_uploads.rb
@@ -1,7 +1,7 @@
 class CreateS3RelayUploads < ActiveRecord::Migration
   def change
     create_table :s3_relay_uploads do |t|
-      t.string :uuid
+      t.binary :uuid, length: 16
       t.text :filename
       t.text :public_url
       t.string :state
