20c26147618134325c13f836e8b930dabd3dcf3b
Kenny Johnston <kjohnston.ca@gmail.com>
2014-10-10 17:07:22 -0700

    Revise uploads migration.

--- db/migrate/20141009000804_create_s3_relay_uploads.rb
+++ db/migrate/20141009000804_create_s3_relay_uploads.rb
@@ -1,12 +1,16 @@
 class CreateS3RelayUploads < ActiveRecord::Migration
   def change
     create_table :s3_relay_uploads do |t|
+      t.string :parent_type
+      t.integer :parent_id
       t.binary :uuid, length: 16
       t.text :filename
       t.text :public_url
       t.string :state
+      t.json :data
       t.datetime :uploaded_at
-      t.datetime :processed_at
+      t.datetime :locked_at
+      t.datetime :imported_at
 
       t.timestamps
     end
