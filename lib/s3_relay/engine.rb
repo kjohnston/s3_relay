88558e82ecff16eebdf2ce1da872b27b6ffa2fc5
Kenny Johnston <kjohnston.ca@gmail.com>
2014-10-10 17:09:41 -0700

    Add s3_relay method to decorate models using it.

--- lib/s3_relay/engine.rb
+++ lib/s3_relay/engine.rb
@@ -1,9 +1,18 @@
 module S3Relay
   class Engine < ::Rails::Engine
     isolate_namespace S3Relay
+
+    initializer "s3_relay.action_controller" do |app|
+      ActiveSupport.on_load :action_controller do
+        helper S3Relay::UploadsHelper
+      end
+    end
   end
 end
 
 require "s3_relay/base"
+require "s3_relay/model"
 require "s3_relay/private_url"
 require "s3_relay/upload_presigner"
+
+ActiveRecord::Base.extend S3Relay::Model
---
+++ lib/s3_relay/model.rb
@@ -0,0 +1,37 @@
+module S3Relay
+  module Model
+
+    def s3_relay(attribute, has_many=false)
+
+      virtual_attribute = "new_#{attribute}_uuids"
+
+      attr_accessor virtual_attribute
+
+      if has_many
+        has_many attribute, as: :parent, class_name: "S3Relay::Upload"
+
+        define_method attribute do
+          S3Relay::Upload.where(parent: self)
+        end
+      else
+        has_one attribute, as: :parent, class_name: "S3Relay::Upload"
+
+        define_method attribute do
+          S3Relay::Upload.where(parent: self).order("uploaded_at DESC").limit(1)
+        end
+      end
+
+      association_method = "associate_#{attribute}"
+
+      after_save "associate_#{attribute}"
+
+      define_method association_method do
+        S3Relay::Upload.where(uuid: send(virtual_attribute))
+          .update_all(parent_type: self.class, parent_id: self.id)
+      end
+
+    end
+
+  end
+
+end
