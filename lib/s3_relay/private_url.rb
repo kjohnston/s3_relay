744906cd2526ac09856b6b2f225d83d98e1208eb
John Dell <john@mutations.ltd>
2017-06-21 21:52:48 -0700

    Use `Addressable` gem for URI.escape (fix deprecation warning)

--- lib/s3_relay/private_url.rb
+++ lib/s3_relay/private_url.rb
@@ -1,10 +1,12 @@
+require "addressable"
+
 module S3Relay
   class PrivateUrl < S3Relay::Base
 
     attr_reader :expires, :path
 
     def initialize(uuid, file, options={})
-      filename = URI.encode(file).gsub("+", "%2B")
+      filename = Addressable::URI.escape(file).gsub("+", "%2B")
       @path    = [uuid, filename].join("/")
       @expires = (options[:expires] || 10.minutes.from_now).to_i
     end
